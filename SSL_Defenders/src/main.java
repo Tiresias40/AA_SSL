0178f743db148506eeb5af83e94ee56f1899a5a0
louis <louis.demoulins@etu.u-bordeaux.fr>
2018-10-21 14:44:50 +0200

    supression du main.java

--- SSL_Defenders/src/main.java
+++ SSL_Defenders/src/main.java
@@ -1,4 +1,13 @@
+import IOManager.InputJSONManager;
 
-public class main {
+public class Main {
+    public static void main(String args[])
+    {
+        InputJSONManager ijm = new InputJSONManager();
+        try{
+            ijm.readJsonFromFile("/Users/louis/Documents/ssl_defender_viewer/configs/basic_problem_1.json");
+        }
+        catch (Exception e) { e.getMessage(); }
 
+    }
 }
