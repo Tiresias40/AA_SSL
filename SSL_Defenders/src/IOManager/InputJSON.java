17f4c52579d1a6940079d70865de84b1ed2451f2
Lucas Ghibert <lghibert@emi.u-bordeaux1.fr>
2018-10-25 10:00:54 +0200

    travail continu sur json

--- SSL_Defenders/src/IOManager/InputJSON.java
+++ SSL_Defenders/src/IOManager/InputJSON.java
@@ -1,14 +1,17 @@
 package IOManager;
 
+import graphManagement.Goal;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
-import graphManagement.Goal;
-
-import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.awt.geom.Point2D;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class InputJSON {
 	private ArrayList<Point2D> fieldLimits;
@@ -21,6 +24,16 @@ public class InputJSON {
 	public InputJSON (String filePath) {
 		JSONObject jObj = getJsonFromFile(filePath);
 		
+		fieldLimits = new ArrayList<Point2D>();
+		JSONArray fields = jObj.getJSONArray("fields_limits");
+		Iterator i = fields.iterator();
+        while (i.hasNext()) {
+            JSONObject field = (JSONObject) i.next();
+        }
+		
+		robotRadius = (double) jObj.get("robot_radius");
+		thetaStep = (double) jObj.get("theta_step");
+		posStep = (double) jObj.get("pos_step");
 	}
 	
 	private JSONObject getJsonFromFile (String filePath) {
@@ -39,5 +52,53 @@ public class InputJSON {
         
         return jObj;
     }
+	
+	public ArrayList<Point2D> getFieldLimits() {
+		return fieldLimits;
+	}
+
+	public void setFieldLimits(ArrayList<Point2D> fieldLimits) {
+		this.fieldLimits = fieldLimits;
+	}
+
+	public ArrayList<Goal> getGoals() {
+		return goals;
+	}
+
+	public void setGoals(ArrayList<Goal> goals) {
+		this.goals = goals;
+	}
+
+	public ArrayList<Point2D> getOpponents() {
+		return opponents;
+	}
+
+	public void setOpponents(ArrayList<Point2D> opponents) {
+		this.opponents = opponents;
+	}
+
+	public double getRobotRadius() {
+		return robotRadius;
+	}
+
+	public void setRobotRadius(double robotRadius) {
+		this.robotRadius = robotRadius;
+	}
+
+	public double getThetaStep() {
+		return thetaStep;
+	}
+
+	public void setThetaStep(double thetaStep) {
+		this.thetaStep = thetaStep;
+	}
+
+	public double getPosStep() {
+		return posStep;
+	}
+
+	public void setPosStep(double posStep) {
+		this.posStep = posStep;
+	}
 	
 }
