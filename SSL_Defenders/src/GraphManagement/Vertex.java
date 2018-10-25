22a666e5e803d57bb35f6959547732b9fb8440c1
louis <louis.demoulins@etu.u-bordeaux.fr>
2018-10-25 09:19:08 +0200

    Modification du graphBuilder et de la creation de vertex

--- SSL_Defenders/src/GraphManagement/Vertex.java
+++ SSL_Defenders/src/GraphManagement/Vertex.java
@@ -3,7 +3,12 @@ package GraphManagement;
 import java.awt.geom.Point2D;
 
 public class Vertex {
-    public String name;
-    public Point2D location;
+    public Point2D.Double location;
     public VertexType type;
+
+    public Vertex(double x, double y)
+    {
+        location = new Point2D.Double(x, y);
+        type = VertexType.NONE;
+    }
 }
