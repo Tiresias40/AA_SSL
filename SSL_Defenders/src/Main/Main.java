262ceca8e90b0ae599e7f70bf0e8889458e3ea02
mperroy <mathieu.perroy@gmail.com>
2018-11-26 14:13:17 +0100

    Ajout de tous les problèmes et toutes les solutions

--- SSL_Defenders/src/Main/Main.java
+++ SSL_Defenders/src/Main/Main.java
@@ -9,7 +9,7 @@ import graphManagement.GraphBuilder.BasicGraphBuilder;
 public class Main {
     public static void main(String args[])
     {
-        InputJSON input = InputJSON.getInstance("../data_json/basic_problem_1.json");
+        InputJSON input = InputJSON.getInstance("../data_json/problem/basic_problem_1.json");
         Graph<Vertex, Edge> g = BasicGraphBuilder.buildGraph(input);
         ExactDominantSetSolver dss = new ExactDominantSetSolver(g);
         boolean result = dss.hasDominatingSet(1);
